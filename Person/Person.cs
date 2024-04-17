80e565b333d916431e9347cf8d31f8fe28160ccf
Hristian2314 <f75@abv.bg>
2024-04-17 09:42:50 +0300

    First commit

---
+++ Person/Person.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Person
+{
+    public class Person
+    {
+        public string name;
+        public int age;
+
+        public void IntroduseYoureSelf()
+        {
+            Console.WriteLine($" az sum {this.name} i sum na {this.age}");
+        }
+    }
+}
---
+++ Person/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Person
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person chovek = new Person();
+
+            chovek.name = "Ivan";
+
+            chovek.age = -15;
+            chovek.IntroduseYoureSelf();
+           
+        }
+    }
+}
